7307fdb87a35a340648d6e1d27dd41c0b904ddd9
Cesar <bitcoinausa@gmail.com>
2024-04-26 16:51:43 -0300

    headers in all paths updated, index responsive

--- scripts/divsHTML.js
+++ scripts/divsHTML.js
@@ -10,30 +10,30 @@ const newContainerBannerHTML = (object) => {
     if (id % 2 === 1) {
         firstCardBanner = `
         <div class="container-fluid p-0 card text-bg-dark">
-            <img class="id="banner-${id} img-fluid" src="${posterBg}" alt="${title}">
+            <img class="img-fluid fixed-height" id="banner-${id}" src="${posterBg}" alt="${title}">
 
-            <div class="card-img-overlay ps-5 d-flex justify-content-start align-items-center z-1">
-                <div class="col-6 card-movie">
-                    <div class="card mb-3 d-block shadow-lg bg-dark bg-opacity-50">
+            <div class="card-img-overlay ps-xl-5 d-flex justify-content-xl-start justify-content-center align-items-center align-items-xl-center z-1">
+                <div class="col-md-6 card-movie">
+                    <div class="mb-3 d-block shadow-lg bg-dark bg-opacity-50">
                         <div class="row g-0">
-                            <div class="col-4 d-flex">
-                                <img class="img-fluid rounded-start" src="${poster}" alt="${title}">
+                            <div class="col-md-4 d-flex justify-content-center">
+                                <img class="img-fluid rounded-1" src="${poster}" alt="${title}">
                             </div>                         
-                            <div class="col-8">
+                            <div class="col-md-8 description">
                                 <div class="card-body text-white pb-0">
                                     <h1 class="card-title fw-bold m-0 mb-4 text-start">${title}</h1>
-                                    <div class="row align-items-center">
-                                        <div class="col-8 text-start pb-4 d-grid">
+                                    <div class="row align-items-center align-baseline">
+                                        <div class="col-xl-9 text-start pb-4 d-grid">
                                             <h6 class="m-0 fw-bolder">Release date:</h6>
                                             <p class="card-text">${year}</p>
                                             <h6 class="m-0 fw-bolder">Duration:</h6>
-                                            <small></small>
+                                            <small>1h 30min</small>
                                             <h6 class="card-title mt-3 mb-0 fw-bolder">Overview</h6>
                                             <p class="card-text">${overview}</p>
                                         </div>
-                                        <div class="col-4 text-center">
+                                        <div class="col-xl-3 col-sm text-center">
                                             <h1 class="card-title">Rate</h1>
-                                            <div class="d-inline-flex justify-content-center">
+                                            <div class="d-inline-flex justify-content-center mb-2">
                                                 <div class="d-flex justify-content-center align-items-center rounded-bottom rate">
                                                     <h3 class="card-text">${rate}</h3>
                                                 </div>
@@ -46,50 +46,49 @@ const newContainerBannerHTML = (object) => {
                     </div>
                 </div>
             </div>                     
-        </div>    
+        </div>  
     `;
     
     } else {
         firstCardBanner = `
         <div class="container-fluid p-0 card text-bg-dark">
-            <img class="id="banner-${id} img-fluid" src="${posterBg}" alt="${title}">
+            <img class="img-fluid fixed-height" id="banner-${id}" src="${posterBg}" alt="${title}">
 
-            <div class="card-img-overlay pe-5 d-flex justify-content-end align-items-center z-1">
-                <div class="col-6 card-movie">
-                    <div class="card mb-3 d-block shadow-lg bg-dark bg-opacity-50">
-                        <div class="row g-0">                            
-                            <div class="col-8">
+            <div class="card-img-overlay ps-xl-5 d-flex justify-content-xl-end justify-content-center align-items-center align-items-xl-center z-1">
+                <div class="col-md-6 card-movie">
+                    <div class="mb-3 d-block shadow-lg bg-dark bg-opacity-50">
+                        <div class="row g-0">                        
+                            <div class="col-md-8 description">
                                 <div class="card-body text-white pb-0">
                                     <h1 class="card-title fw-bold m-0 mb-4 text-end">${title}</h1>
-                                    <div class="row align-items-center">
-                                        <div class="col-4 text-center">
+                                    <div class="row align-items-center align-baseline">
+                                        <div class="col-xl-3 col-sm text-center">
                                             <h1 class="card-title">Rate</h1>
-                                            <div class="d-inline-flex justify-content-center">
+                                            <div class="d-inline-flex justify-content-center mb-2">
                                                 <div class="d-flex justify-content-center align-items-center rounded-bottom rate">
                                                     <h3 class="card-text">${rate}</h3>
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="col-8 text-end pb-4 d-grid">
+                                        <div class="col-xl-9 text-start pb-4 d-grid">
                                             <h6 class="m-0 fw-bolder">Release date:</h6>
                                             <p class="card-text">${year}</p>
                                             <h6 class="m-0 fw-bolder">Duration:</h6>
-                                            <small></small>
+                                            <small>1h 30min</small>
                                             <h6 class="card-title mt-3 mb-0 fw-bolder">Overview</h6>
                                             <p class="card-text">${overview}</p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-4 d-flex">
-                                <img class="img-fluid rounded-start" src="${poster}" alt="${title}">
-                            </div>
+                            <div class="col-md-4 d-flex justify-content-center">
+                                <img class="img-fluid rounded-1" src="${poster}" alt="${title}">
+                            </div> 
                         </div>
                     </div>
                 </div>
             </div>                     
-        </div>
-    
+        </div>     
     `;
     }
                 
@@ -99,35 +98,34 @@ const newContainerBannerHTML = (object) => {
 
 const newContainerMainHTML = (object) =>{
 
-    const {id, poster, title, year, rate} = object;
+    const {id, poster, posterBg, title, year, overview, rate} = object;
+    // const {id, poster, title, year, rate } = object;
 
     const colSeisCardMovie = document.createElement('div');
-    colSeisCardMovie.classList.add('col-6', 'card-movie');
+    colSeisCardMovie.classList.add('col-lg-6', 'card-movie');
     colSeisCardMovie.id = `card-${id}`;
 
     const divCardMain = `
         <div class="card m-2 mx-auto d-block shadow-lg bg-white bg-opacity-75">
             <div class="row g-0">
-                <div class="col-4">
+                <div class="col-lg-4 d-flex justify-content-center">
                     <img class="img-fluid rounded-start" src="${poster}" alt="${title}">
                 </div>
-                <div class="col-8">
+                <div class="col-lg-8">
                     <div class="card-body">
                         <h3 class="card-title fw-bold m-0 mb-4">${title}</h3>
                         <div class="row align-items-center">
-                            <div class="col-8">
+                            <div class="col-9">
                                 <h6 class="m-0 fw-bolder">Release date:</h6>
                                 <p class="card-text">${year}</p>
-                                <h6 class="m-0 fw-bolder">Duration:</h6>
-                                <small class="text-body-secondary"></small>
-                                <h6 class="card-title mt-3 mb-0 fw-bolder">Director</h6>
-                                <p class="card-text">$</p>
+                                <h6 class="card-title mt-3 mb-0 fw-bolder">Descripcion:</h6>
+                                <p class="card-text-vw">${overview}</p>
                             </div>
-                            <div class="col-4 text-center">
+                            <div class="col-3 text-center">
                                 <h2 class="card-title">Rate</h2>
                                 <div class="d-inline-flex justify-content-center">
                                     <div class="d-flex justify-content-center align-items-center rounded-bottom rate">
-                                        <h3 class="card-text">${rate}</h3>
+                                        <h3 class="card-text card-text">${rate}</h3>
                                     </div>
                                 </div>
                             </div>
