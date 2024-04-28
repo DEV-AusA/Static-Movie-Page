c8bd32c8e2e387c778187df170106f4c36f33ba2
Cesar <bitcoinausa@gmail.com>
2024-04-28 13:24:04 -0300

    video position in card Sgue fixed

--- scripts/divsHTML.js
+++ scripts/divsHTML.js
@@ -187,7 +187,7 @@ const newCardBannerSugeHTML = (object) => {
 
     let cardBannerSuge = ``;
 
-    if (id % 2 === 1) {
+    // if (id % 2 === 1) {
         cardBannerSuge = `
         <div class="container-fluid row p-0 m-0 container-card-bg position-relative">                
             <div class="col-lg-4 ps-lg-5 mt-1 d-flex align-items-center z-1">
@@ -231,54 +231,54 @@ const newCardBannerSugeHTML = (object) => {
             </div>
         </div>
     
-    `;
+        `;
     
-    } else {
-        cardBannerSuge = `
-        <div class="container-fluid row p-0 m-0 container-card-bg position-relative">
-            <div class="col-lg-8 p-0 z-0 banner-img img-carousel-${id}">
-                <div id="banner-${id}" class="w-100 h-100 img-fluid">
-                </div>
-            </div>                
-            <div class="col-lg-4 ps-lg-5 mt-1 d-flex align-items-center z-1">
-                <div class="card-movie">
-                    <div class="card mb-3 mx-auto d-block shadow-lg bg-light bg-opacity-50">
-                        <div class="row g-0">
-                            <div class="col-lg-4 col-md-4 d-inline-flex justify-content-center">
-                                <img class="img-fluid rounded-start" src="${poster}" alt="${title}">
-                            </div>
-                            <div class="col-lg-8 col-md-8">
-                                <div class="card-body text-white pb-0">
-                                    <h3 class="card-title fw-bold m-0 mb-4">${title}</h3>
-                                    <div class="row align-items-center">
-                                        <div class="col-lg-8">
-                                            <h6 class="m-0 fw-bolder">Release date:</h6>
-                                            <p class="card-text">${year}</p>
-                                            <h6 class="m-0 fw-bolder">Duration:</h6>
-                                            <small>${duration}</small>
-                                            <h6 class="card-title mt-3 mb-0 fw-bolder">Director</h6>
-                                            <p class="card-text">${director}</p>
-                                        </div>
+    // } else {
+    //     cardBannerSuge = `
+    //     <div class="container-fluid row p-0 m-0 container-card-bg position-relative">
+    //         <div class="col-lg-8 p-0 z-0 banner-img img-carousel-${id}">
+    //             <div id="banner-${id}" class="w-100 h-100 img-fluid">
+    //             </div>
+    //         </div>                
+    //         <div class="col-lg-4 ps-lg-5 mt-1 d-flex align-items-center z-1">
+    //             <div class="card-movie">
+    //                 <div class="card mb-3 mx-auto d-block shadow-lg bg-light bg-opacity-50">
+    //                     <div class="row g-0">
+    //                         <div class="col-lg-4 col-md-4 d-inline-flex justify-content-center">
+    //                             <img class="img-fluid rounded-start" src="${poster}" alt="${title}">
+    //                         </div>
+    //                         <div class="col-lg-8 col-md-8">
+    //                             <div class="card-body text-white pb-0">
+    //                                 <h3 class="card-title fw-bold m-0 mb-4">${title}</h3>
+    //                                 <div class="row align-items-center">
+    //                                     <div class="col-lg-8">
+    //                                         <h6 class="m-0 fw-bolder">Release date:</h6>
+    //                                         <p class="card-text">${year}</p>
+    //                                         <h6 class="m-0 fw-bolder">Duration:</h6>
+    //                                         <small>${duration}</small>
+    //                                         <h6 class="card-title mt-3 mb-0 fw-bolder">Director</h6>
+    //                                         <p class="card-text">${director}</p>
+    //                                     </div>
 
-                                        <div class="col-lg-4 text-center">
-                                            <h2 class="card-title">Rate</h2>
-                                            <div class="d-inline-flex justify-content-center rate-adjust">
-                                                <div class="d-flex justify-content-center align-items-center bg-warning bg-opacity-25 border border-warning rounded-bottom rate">
-                                                    <h3 class="card-text">${rate}</h3>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>                
-        </div>
+    //                                     <div class="col-lg-4 text-center">
+    //                                         <h2 class="card-title">Rate</h2>
+    //                                         <div class="d-inline-flex justify-content-center rate-adjust">
+    //                                             <div class="d-flex justify-content-center align-items-center bg-warning bg-opacity-25 border border-warning rounded-bottom rate">
+    //                                                 <h3 class="card-text">${rate}</h3>
+    //                                             </div>
+    //                                         </div>
+    //                                     </div>
+    //                                 </div>
+    //                             </div>
+    //                         </div>
+    //                     </div>
+    //                 </div>
+    //             </div>
+    //         </div>                
+    //     </div>
     
-    `;
-    }
+    //      `;
+    // }
                 
     divCardBannerSuge.innerHTML = cardBannerSuge;
     return divCardBannerSuge;
